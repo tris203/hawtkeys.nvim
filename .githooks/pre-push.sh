0e08de776ee8f0467151fe86a5006d39c09ab8f6
tris203 <admin@snappeh.com>
2023-12-17 20:26:23 +0000

    chore: prepush hook

---
+++ .githooks/pre-push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# This hook is called by 'git push' and can be used to prevent a push from taking place.
+#
+
+# Stylua check
+stylua --check lua/ --config-path ./.stylua.toml || exit 1
+echo "Stylua check passed"
+
+# Luacheck
+luacheck lua/ --globals vim || exit 1
+echo "Luacheck passed"
+
+# Echo success
+echo "Pre-push checks passed"
+exit 0
+
