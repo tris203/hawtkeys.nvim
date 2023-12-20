6a6f84b673273b7d4c78a7a0b4c966dbb6578eac
tris203 <admin@snappeh.com>
2023-12-20 23:24:53 +0000

    tests: finish testing

---
+++ tests/hawtkeys/example_configs/function_vim.api.nvim_set_keymap.lua
@@ -0,0 +1,4 @@
+local normalMap = function(lhs, rhs)
+  vim.api.nvim_set_keymap('n', lhs, rhs, { noremap = true, silent = true })
+end
+normalMap('<leader>5', ':echo "hello"<CR>')
--- tests/hawtkeys/ts_spec.lua
+++ tests/hawtkeys/ts_spec.lua
@@ -3,17 +3,11 @@ local ts = require("hawtkeys.ts")
 ---@diagnostic disable-next-line: undefined-field
 local eq = assert.are.same
 
-describe("test", function()
-    it("should return true", function()
-        eq(true, true)
-    end)
-end)
-
 describe("Treesitter can extract keymaps", function()
-before_each(function()
-    require("plenary.reload").reload_module("hawtkeys")
+    before_each(function()
+        require("plenary.reload").reload_module("hawtkeys")
         hawtkeys.setup({})
-end)
+    end)
     it("extract vim.api.nvim_set_keymap()", function()
         local keymap = ts.find_maps_in_file(
             "tests/hawtkeys/example_configs/vim.api.nvim_set_keymap.lua"
@@ -41,23 +35,46 @@ end)
         eq(':lua print("hello")<CR>', keymap[1].rhs)
     end)
 
-        it("Extract short dot index aliasesd keymap", function()
-            hawtkeys.setup({
-                customMaps = {
-                    ["shortIndex.nvim_set_keymap"] = {
-                        modeIndex = 1,
-                        lhsIndex = 2,
-                        rhsIndex = 3,
-                        method = "dot_index_expression",
-                    },
+    it("Extract short dot index aliasesd keymap", function()
+        hawtkeys.setup({
+            customMaps = {
+                ["shortIndex.nvim_set_keymap"] = {
+                    modeIndex = 1,
+                    lhsIndex = 2,
+                    rhsIndex = 3,
+                    method = "dot_index_expression",
                 },
-            })
-            local keymap = ts.find_maps_in_file(
-                "tests/hawtkeys/example_configs/aliased_vim.api.nvim_set_keymap.lua"
-            )
-            eq("n", keymap[1].mode)
-            eq("<leader>4", keymap[1].lhs)
-            eq(':echo "hello"<CR>', keymap[1].rhs)
-        end)
-    end
-)
+            },
+        })
+        local keymap = ts.find_maps_in_file(
+            "tests/hawtkeys/example_configs/aliased_vim.api.nvim_set_keymap.lua"
+        )
+        eq("n", keymap[1].mode)
+        eq("<leader>4", keymap[1].lhs)
+        eq(':echo "hello"<CR>', keymap[1].rhs)
+    end)
+
+    it("Extract function call aliased keymap with a fixed mode", function()
+        hawtkeys.setup({
+            customMaps = {
+                ["normalMap"] = {
+                    modeIndex = 'n',
+                    lhsIndex = 1,
+                    rhsIndex = 2,
+                    method = "function_call",
+                },
+            },
+        })
+        local keymap = ts.find_maps_in_file(
+            "tests/hawtkeys/example_configs/function_vim.api.nvim_set_keymap.lua"
+        )
+        print(vim.inspect(keymap))
+        -- TODO: currently index 2, as the first index is the function itself
+        -- This needs to be fixed
+        eq("n", keymap[2].mode)
+        eq("<leader>5", keymap[2].lhs)
+        eq(':echo "hello"<CR>', keymap[2].rhs)
+    end)
+
+    
+end)
