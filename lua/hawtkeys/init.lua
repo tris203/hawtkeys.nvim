ec888bb714c5b37bc5e78864b01ae0c41c90cad1
tris203 <admin@snappeh.com>
2023-12-20 23:32:28 +0000

    refactor: move command registration

--- lua/hawtkeys/init.lua
+++ lua/hawtkeys/init.lua
@@ -47,22 +47,22 @@ function M.setup(config)
     M.powerFingers = config.powerFingers or { 2, 3, 6, 7 }
     M.keyboardLayout = config.keyboardLayout or "qwerty"
     M.keyMapSet = vim.tbl_extend("force", _defaultSet, config.customMaps or {})
-end
 
-vim.api.nvim_create_user_command(
-    "Hawtkeys",
-    "lua require('hawtkeys.ui').show()",
-    {}
-)
-vim.api.nvim_create_user_command(
-    "HawtkeysAll",
-    "lua require('hawtkeys.ui').show_all()",
-    {}
-)
-vim.api.nvim_create_user_command(
-    "HawtkeysDupes",
-    "lua require('hawtkeys.ui').show_dupes()",
-    {}
-)
+    vim.api.nvim_create_user_command(
+        "Hawtkeys",
+        "lua require('hawtkeys.ui').show()",
+        {}
+    )
+    vim.api.nvim_create_user_command(
+        "HawtkeysAll",
+        "lua require('hawtkeys.ui').show_all()",
+        {}
+    )
+    vim.api.nvim_create_user_command(
+        "HawtkeysDupes",
+        "lua require('hawtkeys.ui').show_dupes()",
+        {}
+    )
+end
 
 return M
