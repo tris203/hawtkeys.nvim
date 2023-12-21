f0783201faa567879c0deef0c1081c3097b40835
Tristan Knight <tris203@gmail.com>
2023-12-21 21:09:58 +0000

    refactor(init): move config into object and add tests (#33)
    Co-authored-by: tris203 <admin@snappeh.com>
    Co-authored-by: Will Hopkins <willothyh@gmail.com>

--- lua/hawtkeys/init.lua
+++ lua/hawtkeys/init.lua
@@ -1,13 +1,18 @@
+---@class Hawtkeys
+---@field config HawtKeyConfig
+---@field package defaultConfig HawtKeyConfig
 local M = {}
 
----@alias SupportedKeyboardLayouts "qwerty" | "dvorak"
+---@alias HawtKeySupportedKeyboardLayouts "qwerty" | "dvorak"
 
 ---@class HawtKeyConfig
 ---@field leader string
 ---@field homerow number
 ---@field powerFingers number[]
----@field keyboardLayout SupportedKeyboardLayouts
+---@field keyboardLayout HawtKeySupportedKeyboardLayouts
+---@field keyMapSet { [string] : TSKeyMapArgs | WhichKeyMapargs } | nil
 ---@field customMaps { [string] : TSKeyMapArgs | WhichKeyMapargs } | nil
+---@field highlights HawtKeyHighlights | nil
 
 ---@class HawtKeyHighlights
 ---@field HawtkeysMatchGreat vim.api.keyset.highlight | nil
@@ -19,13 +24,11 @@ local M = {}
 ---@field leader string | nil
 ---@field homerow number | nil
 ---@field powerFingers number[] | nil
----@field keyboardLayout SupportedKeyboardLayouts | nil
+---@field keyboardLayout HawtKeySupportedKeyboardLayouts | nil
 ---@field customMaps { [string] : TSKeyMapArgs | WhichKeyMapargs } | nil
 ---@field highlights HawtKeyHighlights | nil
----
 
 ---@type { [string] : TSKeyMapArgs | WhichKeyMapargs }---
-
 local _defaultSet = {
     ["vim.keymap.set"] = {
         modeIndex = 1,
@@ -46,58 +49,67 @@ local _defaultSet = {
     }, -- method 6
 }
 
+local defaultConfig = {
+    leader = " ",
+    homerow = 2,
+    powerFingers = { 2, 3, 6, 7 },
+    keyboardLayout = "qwerty",
+    keyMapSet = _defaultSet,
+    highlights = {
+        HawtkeysMatchGreat = { link = "DiagnosticOk", underline = true },
+        HawtkeysMatchGood = { link = "DiagnosticOk" },
+        HawtkeysMatchOk = { link = "DiagnosticWarn" },
+        HawtkeysMatchBad = { link = "DiagnosticError" },
+    },
+}
+
+local function apply_highlights()
+    for name, props in pairs(M.config.highlights) do
+        local styleConfig
+        if props.link then
+            styleConfig = vim.api.nvim_get_hl(0, {
+                name = props.link,
+                link = false,
+            })
+        else
+            styleConfig = {}
+        end
+
+        for k, v in pairs(props) do
+            if k ~= "link" then
+                styleConfig[k] = v
+            end
+        end
+        vim.api.nvim_set_hl(0, name, styleConfig)
+    end
+end
+
 ---@param config HawtKeyPartialConfig
 function M.setup(config)
-    config = config or {}
-    M.leader = config.leader or " "
-    M.homerow = config.homerow or 2
-    M.powerFingers = config.powerFingers or { 2, 3, 6, 7 }
-    M.keyboardLayout = config.keyboardLayout or "qwerty"
-    M.keyMapSet = vim.tbl_extend("force", _defaultSet, config.customMaps or {})
-
-    local default_match_great
-    if not config.highlights or not config.highlights.HawtkeysMatchGreat then
-        default_match_great =
-            vim.api.nvim_get_hl(0, { name = "DiagnosticOk", link = false })
-        default_match_great.underline = true
+    M.config = M.config or {}
+    for k, default in pairs(defaultConfig) do
+        local v = config[k]
+        if k == "highlights" then
+            -- shallow merge to preserve highlight values
+            M.config[k] =
+                vim.tbl_extend("force", defaultConfig.highlights, v or {})
+        elseif k == "keyMapSet" then
+            M.config[k] = vim.tbl_deep_extend(
+                "force",
+                defaultConfig.keyMapSet,
+                config.customMaps or {}
+            )
+        elseif type(default) == "table" then
+            M.config[k] = vim.tbl_deep_extend("force", default, v or {})
+        else
+            M.config[k] = v or default
+        end
     end
-    M.highlights = vim.tbl_extend("keep", config.highlights or {}, {
-        HawtkeysMatchGreat = default_match_great,
-        HawtkeysMatchGood = {
-            link = "DiagnosticOk",
-        },
-        HawtkeysMatchOk = {
-            link = "DiagnosticWarn",
-        },
-        HawtkeysMatchBad = {
-            link = "DiagnosticError",
-        },
-    })
 
-    for name, hl in pairs(M.highlights) do
-        vim.api.nvim_set_hl(0, name, hl)
-    end
+    apply_highlights()
 
     vim.api.nvim_create_autocmd("ColorScheme", {
-        callback = function()
-            if default_match_great then
-                for k, v in
-                    pairs(
-                        vim.api.nvim_get_hl(
-                            0,
-                            { name = "DiagnosticOk", link = false }
-                        )
-                    )
-                do
-                    default_match_great[k] = v
-                end
-                default_match_great.underline = true
-            end
-
-            for name, hl in pairs(M.highlights) do
-                vim.api.nvim_set_hl(0, name, hl)
-            end
-        end,
+        callback = apply_highlights,
     })
 
     vim.api.nvim_create_user_command(
@@ -116,5 +128,13 @@ function M.setup(config)
         {}
     )
 end
+
+setmetatable(M, {
+    __index = function(_, k)
+        if k == "defaultConfig" then
+            return vim.deepcopy(defaultConfig)
+        end
+    end,
+})
 
 return M
