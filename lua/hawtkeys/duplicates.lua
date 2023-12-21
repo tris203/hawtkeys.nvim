683e163909dba3247775f581d9897c540d8245b1
Tristan Knight <tris203@gmail.com>
2023-12-21 23:49:20 +0000

    fix(duplicates): fix data shape (#37)
    Co-authored-by: tris203 <admin@snappeh.com>

--- lua/hawtkeys/duplicates.lua
+++ lua/hawtkeys/duplicates.lua
@@ -7,13 +7,11 @@ function M.show_duplicates()
     local allKeys = tsSearch.get_all_keymaps()
     local duplicates = utils.find_duplicates(allKeys)
     local resultTable = {}
-    for index, data in ipairs(duplicates) do
+    for index, data in pairs(duplicates) do
         table.insert(resultTable, tostring(index) .. " duplicates found in ")
         table.insert(
             resultTable,
-            tostring(data[1][1].from_file)
-                .. ":"
-                .. tostring(data[2][1].from_file)
+            tostring(data[1].from_file) .. ":" .. tostring(data[2].from_file)
         )
     end
     return resultTable
