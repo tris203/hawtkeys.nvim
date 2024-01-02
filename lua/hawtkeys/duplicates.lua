31f0a7ffab395c415a08ce336df2ee27c135ec03
Tristan Knight <admin@snappeh.com>
2024-01-02 03:07:47 +0000

    feat(ui): message on no dupes (#65)

--- lua/hawtkeys/duplicates.lua
+++ lua/hawtkeys/duplicates.lua
@@ -4,8 +4,10 @@ local tsSearch = require("hawtkeys.ts")
 
 ---@class HawtkeysDuplicatesData
 ---@field key string
----@field file1 HawtkeysKeyMapData
----@field file2 HawtkeysKeyMapData
+---@field file1 string
+---@field file2 string
+---
+---TODO: Make this return a HawtkeysKeyMapData instead of strings
 
 ---@return HawtkeysDuplicatesData[]
 function M.show_duplicates()
@@ -13,6 +15,7 @@ function M.show_duplicates()
     local duplicates = utils.find_duplicates(allKeys)
     local resultTable = {}
     for index, data in pairs(duplicates) do
+        ---@type HawtkeysDuplicatesData
         local object = {
             key = index,
             file1 = data[1].from_file,
