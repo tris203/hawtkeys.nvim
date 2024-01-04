5889f181b56ca319cdc7ba777b13b3263a14c50a
tris203 <admin@snappeh.com>
2024-01-04 15:47:02 +0000

    code review

--- lua/hawtkeys/keyboards.lua
+++ lua/hawtkeys/keyboards.lua
@@ -1,6 +1,6 @@
 local M = {}
 -- TODO: Make this dynamic, loading from the keyboards directory
---
+
 ---@alias HawtKeySupportedKeyboardLayouts "qwerty" | "colemak" | "colemak-dh"
 
 M.qwerty = require("hawtkeys.keyboards.qwerty").layout
