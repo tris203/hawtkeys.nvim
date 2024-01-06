990dbd57af7b7eab4aad261aeecfc2acad2e4752
tris203 <admin@snappeh.com>
2024-01-06 13:04:59 +0000

    ci: fix?

--- tests/hawtkeys/e2e_spec.lua
+++ tests/hawtkeys/e2e_spec.lua
@@ -1,12 +1,10 @@
 ---@diagnostic disable-next-line: undefined-field
 local eq = assert.are.same
----@diagnostic disable-next-line: undefined-field
 local truthy = assert.is_true
 local ts = require("hawtkeys.ts")
 
 local function copy_configs_to_stdpath_config()
     local config_dir = vim.fn.stdpath("config")
-    vim.fn.mkdir(config_dir, "p")
     vim.fn.writefile(
         vim.fn.readfile("tests/hawtkeys/example_configs/e2e_config.lua"),
         config_dir .. "/e2e_config.lua"
@@ -21,23 +19,32 @@ describe("file searching", function()
 
     it("can detect a Vim Default Keymap", function()
         local keymaps = ts.get_all_keymaps()
+        print(vim.inspect(keymaps))
         truthy(vim.tbl_contains(keymaps, function(v)
-            return v.from_file == "Vim Defaults"
+            return vim.deep_equal(v, {
+                from_file = "Vim Defaults",
+                lhs = "Y",
+                mode = "n",
+                rhs = "y$",
+            })
         end, { predicate = true }))
     end)
 
     it("can detect a keymap from a file", function()
         local keymaps = ts.get_all_keymaps()
+        print(vim.fn.stdpath("config"))
+        print(vim.inspect(keymaps))
         truthy(vim.tbl_contains(keymaps, function(v)
-            return v.from_file == vim.fn.stdpath("config") .. "/e2e_config.lua"
+            return vim.deep_equal(v, {
+                from_file = vim.fn.stdpath("config") .. "/e2e_config.lua",
+                lhs = "<leader>example",
+                mode = "n",
+                rhs = "<cmd>echo 'Example'<cr>",
+            })
         end, { predicate = true }))
     end)
 
     it("should find a file in stdpath('config')", function()
         eq(true, true)
-    end)
-
-    after_each(function()
-        vim.cmd("silent! !rm " .. vim.fn.stdpath("config") .. "/e2e_config.lua")
     end)
 end)
--- tests/hawtkeys/example_configs/e2e_config.lua
+++ tests/hawtkeys/example_configs/e2e_config.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_set_keymap("n", "<leader>example", "echo 'Example'", {})
+vim.api.nvim_set_keymap("n", "<leader>example", "<cmd>echo 'Example'<cr>", {})
